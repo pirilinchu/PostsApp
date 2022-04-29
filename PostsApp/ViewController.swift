8e359c3087bf2e03c712be3b283904789e0dc03d
Santiago Mendoza <santiagomendozaa6@gmail.com>
2022-04-29 17:42:33 -0400

    Initial Commit

---
+++ PostsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostsApp
+//
+//  Created by Santiago Mendoza on 29/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
